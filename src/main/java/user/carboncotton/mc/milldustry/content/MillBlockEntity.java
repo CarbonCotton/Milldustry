928ee8635e63c649a4446e97d604ef20669b5992
CarbonCotton <carboncotton@proton.me>
2024-04-08 23:06:19 +0200

    Added inventory field and constants

--- src/main/java/user/carboncotton/mc/milldustry/content/MillBlockEntity.java
+++ src/main/java/user/carboncotton/mc/milldustry/content/MillBlockEntity.java
@@ -2,11 +2,22 @@ package user.carboncotton.mc.milldustry.content;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 
 public class MillBlockEntity extends BlockEntity {
 
+	private static final int FUEL_SLOT_INDEX = 0;
+	private static final int INPUT_SLOT_INDEX = 1;
+	private static final int[] OUTPUT_SLOT_INDEX = new int[]{2,3,4,5,6,7,8,9};
+	private DefaultedList<ItemStack> inventory;
+
+
+
 	public MillBlockEntity(BlockPos pos, BlockState state) {
 		super(AllObjects.KILN_BLOCK_ENTITY, pos, state);
+
+		this.inventory = DefaultedList.ofSize(10, ItemStack.EMPTY);
 	}
 }
